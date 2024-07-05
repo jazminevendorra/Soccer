4c537b798561bb1e3ba51456f20b61564b71c8b5
Jazmin <163955648+jazminevendorra@users.noreply.github.com>
2024-07-05 11:06:47 -0400

    Update soccer.cpp

--- soccer.cpp
+++ soccer.cpp
@@ -1,6 +1,5 @@
 // This program will store player information (name, number, and goals scored),
 // and display, and manipulate the information provided from file.
-// Jazmin Even Dorra
 
 
 // Header Files
